f2a4536d74edf9b79e6745f98d5de8fe907d157d
Rahul <rahuldutta.rd11@gmail.com>
2022-01-06 22:09:28 +0530

    gallery

--- src/components/pages/gallery/Gallery.jsx
+++ src/components/pages/gallery/Gallery.jsx
@@ -1,25 +1,47 @@
 import { SRLWrapper } from "simple-react-lightbox";
+import "./gallery.css";
+import Ten from "../../images/10.jpg";
+import Nine from "../../images/9.jpg";
+import Eight from "../../images/8.jpg";
+import Seven from "../../images/7.jpg";
+import Six from "../../images/6.jpg";
+import Five from "../../images/5.jpg";
+
+
 
 
 
 
 export default function Gallery() {
- 
   return (
     <SRLWrapper>
-      <div className="Gallery">
-      <a href="https://picsum.photos/1024/768?image=2">
-         <img src="https://picsum.photos/200/300?image=2" alt="lightbox" />
-       </a>
-       <a href="https://picsum.photos/1024/768?image=3">
-         <img src="https://picsum.photos/200/300?image=3" alt="lightbox1" />
-       </a>
-       <a href="https://picsum.photos/1024/768?image=4">
-         <img src="https://picsum.photos/200/300?image=4" alt="lightbox2" />
-       </a>
-       <a href="https://picsum.photos/1024/768?image=5">
-         <img src="https://picsum.photos/200/300?image=5" alt="lightbox3" />
-       </a>
+      <div className="gallery">
+        <div className="title">Gallery of Brookfield School</div>
+        <div class="outer-grid">
+          <div class="inner-grid">
+            <a href={Ten}>
+            <img src={Ten}  alt="Class 10"/>
+            </a>
+            <a href={Eight}>
+            <img src={Eight}  alt="Class 8"/>
+            </a>
+            <a href={Six}>
+            <img src={Six}  alt="Class 6"/>
+            </a>
+            
+          </div>
+          <div class="inner-grid">
+          <a href={Nine}>
+            <img src={Nine}  alt="Class 9"/>
+            </a>
+            <a href={Seven}>
+            <img src={Seven}  alt="Class 7"/>
+            </a>
+            <a href={Five}>
+            <img src={Five}  alt="Class 5"/>
+            </a>
+          </div>
+        </div>
       </div>
     </SRLWrapper>
   );
