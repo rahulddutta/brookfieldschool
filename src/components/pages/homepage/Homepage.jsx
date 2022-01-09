f7ddb86105492607c0cb98e51a30468207be2c35
Rahul <rahuldutta.rd11@gmail.com>
2022-01-09 20:23:44 +0530

    done

--- src/components/pages/homepage/Homepage.jsx
+++ src/components/pages/homepage/Homepage.jsx
@@ -375,9 +375,9 @@ export default function Homepage() {
               requested to fill in the details with caution. It is to be noted
               that admission is given on first come first served basis.
             </span>{" "}
-            <Link to="/admission" className={classes.addSpan2}>
+            <a className={classes.addSpan2} href="www.google.com">
               Enroll Today
-            </Link>{" "}
+            </a>{" "}
           </div>
         </Grid>
       </Grid>
